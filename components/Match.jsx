a289a4be5b29adc5547efeb42b6b1a9293a654b6
robertjezyk <rjezyk@vp.pl>
2024-03-08 12:06:00 +0000

    internal linking

--- components/Match.jsx
+++ components/Match.jsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import Image from "next/image";
+import Link from "next/link";
 import { VscWorkspaceUnknown } from "react-icons/vsc";
 
 import { DeleteForm } from "@/components/DeleteForm";
@@ -24,10 +25,14 @@ const TeamBadge = ({ team }) => (
 export const Match = ({ match, showDeleteButton }) => {
   const homeTeamWon = match.setsHome > match.setsAway;
   const homeTeamClassName = `flex gap-2 items-center text-lg capitalize ${
-    homeTeamWon ? "text-slate-100" : "text-slate-500"
+    homeTeamWon
+      ? "text-slate-50/90 hover:text-slate-50/100"
+      : "text-slate-400/80 hover:text-slate-400/100"
   }`;
   const awayTeamClassName = `flex gap-2 items-center text-lg capitalize ${
-    !homeTeamWon ? "text-slate-100" : "text-slate-500"
+    !homeTeamWon
+      ? "text-slate-50/90 hover:text-slate-50/100"
+      : "text-slate-400/80 hover:text-slate-400/100"
   }`;
   const triangleClassName =
     "border-t-4 border-b-4 border-r-8 border-r-white border-t-transparent border-b-transparent block w-4 h-2";
@@ -40,22 +45,26 @@ export const Match = ({ match, showDeleteButton }) => {
       </span>
       <div className="flex justify-between gap-2 items-center">
         <div>
-          <h3 className={homeTeamClassName}>
-            {match.homeTeam.badge ? (
-              <TeamBadge team={match.homeTeam} />
-            ) : (
-              <UnknownBadge />
-            )}
-            {match.homeTeam.name}
-          </h3>
-          <h3 className={awayTeamClassName}>
-            {match.awayTeam.badge ? (
-              <TeamBadge team={match.awayTeam} />
-            ) : (
-              <UnknownBadge />
-            )}
-            {match.awayTeam.name}
-          </h3>
+          <Link href={`/teams/${match.homeTeam.id}`}>
+            <h3 className={homeTeamClassName}>
+              {match.homeTeam.badge ? (
+                <TeamBadge team={match.homeTeam} />
+              ) : (
+                <UnknownBadge />
+              )}
+              {match.homeTeam.name}
+            </h3>
+          </Link>
+          <Link href={`/teams/${match.awayTeam.id}`}>
+            <h3 className={awayTeamClassName}>
+              {match.awayTeam.badge ? (
+                <TeamBadge team={match.awayTeam} />
+              ) : (
+                <UnknownBadge />
+              )}
+              {match.awayTeam.name}
+            </h3>
+          </Link>
         </div>
 
         <div className="flex">
--- utils/calculateForm.js
+++ utils/calculateForm.js
@@ -9,10 +9,14 @@ export const calculateForm = (teamId, playedMatches = []) => {
     .filter(
       (match) => match.homeTeamId === teamId || match.awayTeamId === teamId
     )
-    .map((match) =>
-      (match.homeTeamId === teamId && match.setsHome > match.setsAway) ||
-      (match.awayTeamId === teamId && match.setsAway > match.setsHome)
-        ? "W"
-        : "L"
-    );
+    .map((match) => ({
+      id: match.id,
+      outcome:
+        (match.homeTeamId === teamId && match.setsHome > match.setsAway) ||
+        (match.awayTeamId === teamId && match.setsAway > match.setsHome)
+          ? "W"
+          : "L",
+      setScore: "",
+      pointScore: "",
+    }));
 };
